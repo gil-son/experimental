bfd766c674243d823098c3ce504677d3eb34a2f1
Gilson Vieira de Souza <gilszon@gmail.com>
2023-01-09 18:09:22 -0300

    Form - improves

--- matrizero/frontweb-responsive-routes/src/components/Form/index.jsx
+++ matrizero/frontweb-responsive-routes/src/components/Form/index.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import {REACT_APP} from '../../utils/request';
-
+import './style.css';
 
 function FormEmail() {
 
@@ -11,6 +11,8 @@ function FormEmail() {
     let [ phone, setPhone] = useState("");
     let [ subject, setSubject] = useState("");
     let [ message, setMessage] = useState("");
+    let [ theCheckbox, setTheCheckbox] = useState(false);
+    
 
     let [ checkSend, setCheckSend] = useState(true);
 
@@ -20,16 +22,16 @@ function FormEmail() {
     let [ messagePhone, setMessagePhone] = useState(false);
     let [ messageSubject, setMessageSubject] = useState(false);
     let [ messageMessage, setMessageMessage] = useState(false);
+    let [ messageSending, setMessageSending] = useState(false);
 
     let [ count, setCount] = useState(false);
 
     function verify(){
         
-        
 
         if(name.length > 1){count++;}else{setMessageName(true); setTimeout( () => {setMessageName(false)},10000);}
         if(email.length > 7 && email.includes("@")){count++;}else{setMessageEmail(true); setTimeout( () => {setMessageEmail(false)},10000);}
-        if(phone.length > 6){count++;}else{setMessagePhone(true); setTimeout( () => {setMessagePhone(false)},10000);}
+        if(phone.length > 7){count++;}else{setMessagePhone(true); setTimeout( () => {setMessagePhone(false)},10000);}
         if(subject.length > 4){count++;}else{setMessageSubject(true); setTimeout( () => {setMessageSubject(false)},10000);}
         if(message.length > 1){count++;}else{setMessageMessage(true); setTimeout( () => {setMessageMessage(false)},10000);}
 
@@ -37,11 +39,31 @@ function FormEmail() {
         console.log(count)
         count=0;
     }
+
+    function verifyCheckBox(){
+        
+        if(theCheckbox == true){setTheCheckbox(false)}else{setTheCheckbox(true)}
+        console.log("theCheckbox", theCheckbox)
+    }
+
+    function validate(){
+        verifyCheckBox();
+        verify();
+    }
     
+    function waitSecond(){
+        
+        if(theCheckbox){
+            setMessageSending(true)
+            setTimeout( () => {setMessageSending(false)},10000);
+        }
+    }
     
-    
-    
-    return (
+   // https://stackoverflow.com/questions/48213137/when-a-user-click-on-a-submit-button-how-to-wait-1-second-before-activating-it
+   // https://github.com/re-ciclo/re-ciclo/blob/master/frontend/src/components/Form/index.jsx 
+   
+   return (
+        <>
         <form action="https://api.staticforms.xyz/submit" method="post">
             <input type="hidden" name="accessKey" value="647095ca-9257-41dd-9368-63a1bdbb1629"/>
             <input type="hidden" name="replyTo" value="matrizero.dev@gmail.com" />
@@ -65,32 +87,37 @@ function FormEmail() {
             <div>
             
             </div>
-            <div className="form-group mt-2  shadow " onMouseLeave={verify}>
+            <div className="form-group mt-2  shadow " onMouseLeave={verify} onMouseMove={verify}>
             <input type="hidden" name="redirectTo" value={REACT_APP}></input>
             <div className="form-check">
-            <input className="form-check-input" type="checkbox" id="flexCheckDefault" onClick={verify} required/>
+            <input className="form-check-input" type="checkbox" id="flexCheckDefault" onClick={validate} required/>
             <label className="form-check-label" for="flexCheckDefault">
                 Estou ciente do envio do e-mail e em até 1 dias útil serei respondido
             </label>
 </div>
-            <button type="submit" className="btn btn-outline-primary w-100" disabled={checkSend}>Enviar</button>
+            <button type="submit" className="btn btn-outline-primary w-100" disabled={checkSend} onClick={waitSecond}>Enviar</button>
             </div>
+            
                         {
-                            messageName && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around shadow" role="alert">O nome precisa ter o mínimo de 3 caracteres</div>
+                            messageName && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around send-error shadow" role="alert">O nome precisa ter o mínimo de 3 caracteres</div>
                         }
                         {
-                            messageEmail && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around shadow" role="alert">Verifique o e-mail</div>
+                            messageEmail && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around send-error shadow" role="alert">Verifique o e-mail</div>
                         }
                         {
-                            messagePhone && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around shadow" role="alert">Verifique o número de telefone</div>
+                            messagePhone && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around send-error shadow" role="alert">Verifique o número de telefone</div>
                         }
                         {
-                            messageSubject && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around shadow" role="alert">O Assunto precisa ter o mínimo de 5 caracteres</div>
+                            messageSubject && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around send-error shadow" role="alert">O Assunto precisa ter o mínimo de 5 caracteres</div>
                         }
                         {
-                            messageMessage && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around shadow" role="alert">A mensagem precisa ter o mínimo de 10 caracteres</div>
+                            messageMessage && <div className=" d-flex alert alert-dark border border-primary mx-auto my-4 w-100 justify-content-around send-error shadow" role="alert">A mensagem precisa ter o mínimo de 10 caracteres</div>
+                        }
+                        {
+                            messageSending && <div className=" d-flex alert alert-success border border-primary mx-auto my-4 w-100 justify-content-around send-ok shadow" role="alert">Sucesso!</div>
                         }
         </form>
+        </>
     );
 }
 
