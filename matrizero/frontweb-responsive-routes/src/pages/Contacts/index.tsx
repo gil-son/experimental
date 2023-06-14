d27fee9586c28bdb109a28dc00029ac552abef32
gil-son <gilszon@gmail.com>
2023-06-13 22:54:40 -0300

    Organized responsive and icon in guide

--- matrizero/frontweb-responsive-routes/src/pages/Contacts/index.tsx
+++ matrizero/frontweb-responsive-routes/src/pages/Contacts/index.tsx
@@ -5,14 +5,14 @@ import './styles.css';
 
 const Contacts = () => {
   return <>
-    <div className="container mt-5 shadow vh-100 mb-5">
+    <div className="container mt-2 shadow mb-5">
       <div className="row">
         <div className="col-sm mt-2 mb-2">
           <div> <Network /></div>
           <div><FormEmail /></div>
         </div>
-        <div className="col-sm mt-2 d-flex justify-content-center align-items-center">
-        <iframe src="https://giphy.com/embed/xIkOdhhbVsHQoI91vH"  frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+        <div className="col-sm mt-2 mb-5 d-flex justify-content-center align-items-center">
+        <iframe src="https://giphy.com/embed/xIkOdhhbVsHQoI91vH" className="giphy-embed" allowFullScreen></iframe>
         </div>
       </div>
     </div>
