ed9da319b355e52128d82bcb0e7e59055c8a9fe1
gil-son <gilszon@gmail.com>
2023-08-01 17:20:04 -0300

    Reducer with languages  in Radar

--- matrizero/frontweb-responsive-routes/src/components/Progress/index.jsx
+++ matrizero/frontweb-responsive-routes/src/components/Progress/index.jsx
@@ -1,79 +1,67 @@
 import { Radar } from "react-chartjs-2";
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 export const RadarChartWorks = () => {
+  const state = useSelector(state => state);
+  const verify = JSON.stringify(state);
+  const obj = JSON.parse(verify);
 
-  let nacional = [40, 10, 23, 8, 17, 12];
-  let internacional = [20, 15, 11, 10, 10, 14];
+  const nacional = [40, 10, 23, 8, 17, 12];
+  const internacional = [20, 15, 11, 10, 10, 14];
 
+  const [language, setLanguage] = useState(obj.mylanguage.language);
+
+  const arrayPt = ['BANCOS', 'E-COMMERCE', 'STARTUPS', 'OUTROS', 'FREELANCER', 'STREAMS'];
+  const arrayEng = ['BANKS', 'E-COMMERCE', 'STARTUPS', 'OTHERS', 'FREELANCER', 'STREAMS'];
+
+  const infoNacionalPt = '% DE ALUNOS NACIONAIS TRABALHAM EM'
+  const infoInternacionalPt = '% DE ALUNOS INTERNACIONAIS TRABALHAM EM'
+
+  const infoNacionalEng = '% OF NATIONAL STUDENTS WORK IN'
+  const infoInternacionalEng = '% OF INTERNATIONAL STUDENTS WORK IN'
+
+  const array = language ? arrayEng : arrayPt;
+
+  const infoNacional = language ?  infoNacionalEng : infoNacionalPt;
+  const infoInternacional = language ? infoInternacionalEng : infoInternacionalPt;
+
+
+  useEffect(() => {
+    setLanguage(obj.mylanguage.language);
+  }, [obj.mylanguage.language]);
 
   let data = {
-      labels: [
-          'BANCOS',
-          'E-COMMERCE',
-          'STARTUPS',
-          'OUTROS',
-          'FREELANCER',
-          'STREAMS'
-      ],
-      datasets: [
-          {
-              label: '% DE ALUNOS NACIONAIS TRABALHAM EM',
-              data: nacional,
-              backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(14, 102, 135, 0.2)'
-
-              ],
-              borderColor: ['rgba(54, 162, 235, 0.2)', 'rgba(14, 102, 135, 0.2)'
-
-
-              ]
-          },
-          {
-            label: '% DE ALUNOS INTERNACIONAIS TRABALHAM EM',
-            data: internacional,
-            backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(44, 82, 235, 0.2)'
-
-            ],
-            borderColor: ['rgba(54, 162, 235, 0.2)', 'rgba(44, 82, 235, 0.2)'
-
-
-            ]
-        }
-      ]
-  }
+    labels: array,
+    datasets: [
+      {
+        label: infoNacional,
+        data: nacional,
+        backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(14, 102, 135, 0.2)'],
+        borderColor: ['rgba(54, 162, 235, 0.2)', 'rgba(14, 102, 135, 0.2)'],
+      },
+      {
+        label: infoInternacional,
+        data: internacional,
+        backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(44, 82, 235, 0.2)'],
+        borderColor: ['rgba(54, 162, 235, 0.2)', 'rgba(44, 82, 235, 0.2)'],
+      },
+    ],
+  };
 
   const options = {
-      // title:{
-      //     display: true,
-      //     text: 'Pintura dos Apartamentos. Meta 2000 R$'
-      //  },
-      // scales:{
-      //     yAxes: [
-      //         {
-      //             ticks:{
-      //                 min: 0,
-      //                 max: 300,
-      //                 stepSize: 20
-      //             }
-      //         }
-      //     ]
-      //  },
-      scale: {
-        ticks: {
-          font: {
-            weight: 'bold'
-          }
-        }
-      }
-  }
+    scale: {
+      ticks: {
+        font: {
+          weight: 'bold',
+        },
+      },
+    },
+  };
 
   return (
-      
-          <Radar data={data}
-              options={options}
-
-          />
-      
-  )
-}
+    <Radar data={data} options={options} />
+  );
+};
 
 export default RadarChartWorks;
