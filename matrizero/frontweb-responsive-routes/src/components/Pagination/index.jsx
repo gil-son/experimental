c834aa115adb6ab1a08fd01513ab7ada419e933c
gil-son <gilszon@gmail.com>
2023-08-18 01:45:36 -0300

    Rating

--- matrizero/frontweb-responsive-routes/src/components/Pagination/index.jsx
+++ matrizero/frontweb-responsive-routes/src/components/Pagination/index.jsx
@@ -5,7 +5,15 @@ import Pg2 from './pag2.json';
 import Pg3 from './pag3.json';
 import Pg4 from './pag4.json';
 import Pg5 from './pag5.json';
-
+import Preview from '../../assets/images/previews.png';
+import Next from '../../assets/images/next.png';
+import StarEmpty16 from '../../assets/images/star-empty-16.png';
+import StarHalf16 from '../../assets/images/star-half-16.png';
+import StarAll16 from '../../assets/images/star-dark-blue-full-16.png';
+import StarEmpty32 from '../../assets/images/star-empty-24.png';
+import StarHalf32 from '../../assets/images/star-dark-blue-half-24.png';
+import StarAll32 from '../../assets/images/star-dark-blue-full-24.png';
+import './style.css';
 
 export const ReviewsPagination = () => {
   const [reviews, setReviews] = useState([]);
@@ -40,9 +48,6 @@ export const ReviewsPagination = () => {
       })
       .catch(error => console.error(error));
       */
-
-      
-      
       
   }, [currentPage]);
 
@@ -58,40 +63,63 @@ export const ReviewsPagination = () => {
   let items = [];
   for (let number = 1; number <= 5; number++) {
     items.push(
+
+      
       <Pagination.Item onClick={ () => handlePageChange(number)} key={number} active={number === currentPage}>
         {<>{number}</>}
       </Pagination.Item>
     );
   }
 
+  const total = (Pg1.data[0].rating + Pg1.data[1].rating + Pg1.data[2].rating + Pg1.data[3].rating
+  +Pg2.data[0].rating + Pg2.data[1].rating + Pg2.data[2].rating + Pg2.data[3].rating
+  +Pg3.data[0].rating + Pg3.data[1].rating + Pg3.data[2].rating + Pg3.data[3].rating
+  +Pg4.data[0].rating + Pg4.data[1].rating + Pg4.data[2].rating + Pg4.data[3].rating
+  +Pg5.data[0].rating + Pg5.data[1].rating + Pg5.data[2].rating + Pg5.data[3].rating) / 20;
+
   return (
     <div>
-      <h1>Avaliações:</h1>
+      <div>
+        <span className='rating-title'>{total}</span>
+        <img src={StarAll32} /><img src={StarAll32} /><img src={StarAll32} /><img src={StarAll32} /><img src={StarHalf32} />
+         <span className='evaluation-count'>avaliações (20)</span>
+      </div>
+
 
       <div className="row">
         {reviews.map((data, index) => (
-            <div key={index} class="col-12 col-sm-3 box3 border border-primary">
-                <h2>{data.name}</h2>
-                <p>{data.message}</p>
+            <div key={index} class="col-12 col-sm-3 box3 border border-primary rating-card">
+                <h2 className='evaluation-name'>{data.name}</h2>
+                <p className='space-message-rating-card'>{data.message}</p>
+                <div className='d-flex justify-content-between'>
+                <span>
+                  {data.rating == 5 && <><img src={StarAll16} /><img src={StarAll16} /><img src={StarAll16} /><img src={StarAll16} /><img src={StarAll16} /></>}
+                  {data.rating == 4 && <><img src={StarAll16} /><img src={StarAll16} /><img src={StarAll16} /><img src={StarAll16} /><img src={StarEmpty16} /></>}
+                  {data.rating == 3 && <><img src={StarAll16} /><img src={StarAll16} /><img src={StarAll16} /><img src={StarEmpty16} /><img src={StarEmpty16} /></>}
+                  {data.rating == 2 && <><img src={StarAll16} /><img src={StarAll16} /><img src={StarEmpty16} /><img src={StarEmpty16} /><img src={StarEmpty16} /></>}
+                  {data.rating == 1 && <><img src={StarAll16} /><img src={StarEmpty16} /><img src={StarEmpty16} /><img src={StarEmpty16} /><img src={StarEmpty16} /></>}
+                 
+                  
+                </span>
+                <span> {data.date}</span>
+                </div>
             </div>
         ))}
         
      </div>
      
-     {/*
-      <div>
-        <button onClick={() => handlePageChange(currentPage - 1)}>Previous</button>
-        <span>Page {currentPage} of {totalPages}</span>
-        <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
-      </div>
-      */
-    }
-
-    <div>
+     
+      <div className='buttons mt-4'>
+        <img src={Preview} className='previous mb-3' onClick={() => handlePageChange(currentPage - 1)} />
         <Pagination size="sm">{items}</Pagination>
-    </div>
+        <img src={Next} className='next mb-3' onClick={() => handlePageChange(currentPage + 1)}/>
+        <span className='pageof'>Page {currentPage} of {totalPages}</span>
+      </div>
+
+    
     </div>
 
     
   );
 }
+
