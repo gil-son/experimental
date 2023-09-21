4466d145e0ea841ce600d536905afcb10389a2ae
gil-son <gilszon@gmail.com>
2023-09-21 20:56:14 -0300

    Removed pages and components about programacao and linguas

--- matrizero/v002/src/Routes.tsx
+++ matrizero/v002/src/Routes.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
-import Programacao from 'pages/Programacao';
-import Linguas from 'pages/Linguas';
 import Contacts from 'pages/Contacts';
 
 const Routes = () => (
@@ -12,12 +10,6 @@ const Routes = () => (
     <Switch>
       <Route path="/" exact>
         <Home />
-      </Route>
-      <Route path="/programacao">
-        <Programacao />
-      </Route>
-      <Route path="/linguas">
-        <Linguas />
       </Route>
       <Route path="/sobre">
         <Contacts />
